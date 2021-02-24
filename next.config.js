0e51eba2b6b46c8cf42ec4e0a3c9f3236c7dcbac
thelimpbacon <61944046+thelimpbacon@users.noreply.github.com>
2021-02-24 09:31:36 +0100

    revert nextconfig to using module.exports because ES6 is not working on vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-export const images = {
-  domains: [
-    "mount-then-bike-bohol.s3-ap-southeast-1.amazonaws.com",
-    "res.cloudinary.com",
-  ],
+module.exports = {
+  images: {
+    domains: [
+      "mount-then-bike-bohol.s3-ap-southeast-1.amazonaws.com",
+      "res.cloudinary.com",
+    ],
+  },
 };
