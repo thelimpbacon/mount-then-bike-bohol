3aee683f774f36c81bd4c17b2cc68617c8268ba1
thelimpbacon <61944046+thelimpbacon@users.noreply.github.com>
2021-03-01 08:30:57 +0100

    slide indicator is now avalaible on all sizes
    arrows will only show starting on sm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "@lib/apolloClient/client";
 import "../styles/globals.css";
 import "keen-slider/keen-slider.min.css";
-import "../styles/rdu.css";
 
 function MyApp({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState);
