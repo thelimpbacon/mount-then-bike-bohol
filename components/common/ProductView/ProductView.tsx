788d75793278ea18f7995234750d2de2f05f6d45
thelimpbacon <61944046+thelimpbacon@users.noreply.github.com>
2021-03-01 09:39:13 +0100

    styling changes in footer and product view

--- components/common/ProductView/ProductView.tsx
+++ components/common/ProductView/ProductView.tsx
@@ -12,7 +12,7 @@ const Product = ({
 }: ProductType) => {
   return (
     <div className={s.root}>
-      <div className="w-full bg-gray-200 lg:rounded-md lg:my-1 lg:px-1 lg:w-2/3">
+      <div className="w-full bg-gray-200 lg:rounded-md lg:my-0 lg:px-1 lg:w-2/3">
         <ProductSlider>
           {[...[mainImage], ...secondaryImage]?.map((data, i) => (
             <div key={data.public_id} className="lg:p-1 ">
@@ -30,15 +30,15 @@ const Product = ({
           ))}
         </ProductSlider>
       </div>
-      <div className="w-full px-5 my-1 lg:w-1/3 lg:py-20">
-        <div className="my-3 text-2xl font-semibold">{name}</div>
-        <span className="my-4 text-xl ">
-          {price.toLocaleString("en-GB", {
+      <div className="w-full px-5 mt-5 lg:w-1/3 lg:mt-0 ">
+        <div className="my-3 text-3xl font-semibold lg:text-4xl">{name}</div>
+        <div className="my-4 text-xl ">
+          {new Intl.NumberFormat("en-PH", {
             style: "currency",
             currency: "Php",
-          })}
-        </span>
-        <div className="mt-6 whitespace-pre-wrap">{description}</div>
+          }).format(price)}
+        </div>
+        <p className="mt-6 whitespace-pre-wrap">{description}</p>
       </div>
     </div>
   );
