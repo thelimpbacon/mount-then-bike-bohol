5edda363245dd05ffa3f39e30de264bd740556ed
thelimpbacon <61944046+thelimpbacon@users.noreply.github.com>
2021-03-28 11:38:33 +0200

    add node and npm version on package.json
    change footer details
    
    margin change in homepage

--- components/common/Icons/index.ts
+++ components/common/Icons/index.ts
@@ -1,6 +1,4 @@
 export { default as Facebook } from "./Facebook";
-export { default as Instagram } from "./Instagram";
-export { default as Twitter } from "./Twitter";
 export { default as MapPin } from "./MapPin";
 export { default as Phone } from "./Phone";
 export { default as Mail } from "./Mail";
